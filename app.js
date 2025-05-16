4e3bbe0cfe826f814221cedd036f813740e3b664
petraalpigiano <petraalpigiano@gmail.com>
2025-05-16 16:16:46 +0200

    add notFound

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { port } from "./data/db.js";
 import postsRouter from "./routers/posts.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import notFoundHandler from "./middlewares/notFoundHandler.js";
 
 // .EXPRESS
 const app = express();
@@ -22,4 +23,4 @@ app.use("/posts", postsRouter);
 // MIDDLEWARE PER LA GESTIONE DEGLI ERRORI DEL SERVER
 app.use(errorHandler);
 // MIDDLEWARE PER LA GESTIONE DELLE ROTTE NON REGISTRATE
-// app.use();
+app.use(notFoundHandler);
